bcd4698a06e71168b03e73a3db5ae3c29cf61039
Claire Thibodeaux <clairethibodeaux@berkeley.edu>
2021-01-03 23:17:29 -0800

    Updates

---
+++ precache-manifest.bab2568014f96635f8581079b9c63fb6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b022dc1d0ec40360d2e28bd6b9202d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d13d5124d87296c6f3",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "9d2d091be9e2f7e975fe",
+    "url": "/static/css/main.dc52ab03.chunk.css"
+  },
+  {
+    "revision": "a5d13d5124d87296c6f3",
+    "url": "/static/js/2.726a9c1c.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.726a9c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2d091be9e2f7e975fe",
+    "url": "/static/js/main.be77a080.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "cc01f70f4ff90861fd136182a99c0f24",
+    "url": "/static/media/email.cc01f70f.png"
+  },
+  {
+    "revision": "fb7a3851cb608e0a5f73de44c9f329e7",
+    "url": "/static/media/insta.fb7a3851.png"
+  },
+  {
+    "revision": "2aa06469d67d4aebba68e4bf820be206",
+    "url": "/static/media/me.2aa06469.JPG"
+  },
+  {
+    "revision": "ae5fc228e551e54a0cd602edd9cff9f2",
+    "url": "/static/media/pastelback.ae5fc228.jpg"
+  },
+  {
+    "revision": "f8f4ac8af08971adb22333be67dc7aa2",
+    "url": "/static/media/resume.f8f4ac8a.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.8dff2dbb4cb1b442a5f80fbfc05d33c4.js"
+  "/precache-manifest.bab2568014f96635f8581079b9c63fb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
